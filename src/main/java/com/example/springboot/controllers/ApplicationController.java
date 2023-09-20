630eb02487eb6adc9bf2765521f6c95b9a71faa4
saulf01 <saulfloresccv01@gmail.com>
2023-09-20 15:33:20 -0600

    first commit

---
+++ src/main/java/com/example/springboot/controllers/ApplicationController.java
@@ -0,0 +1,25 @@
+package com.example.springboot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+    
+    @GetMapping("/login")
+    public String Login(){
+        return "login";
+    }
+
+    @GetMapping("/dashboard")
+    public String GetDashboard(){
+        return "login";
+    }
+
+    @GetMapping("/main")
+    public String GetMain(){
+        return "main";
+    }
+
+    
+}
